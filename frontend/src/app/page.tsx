8d9116836266aee4558ef48a770d59cb1029ebdf
eric.cheah <eric.cheah@ufinity.com>
2024-03-07 11:33:20 +0800

    feat(web): date picker component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Calendar } from "@/components/ui/calendar";
+import { DatePicker } from "@/components/ui/date-picker";
 import { useTheme } from "next-themes";
 import { useState } from "react";
 
@@ -7,12 +7,7 @@ export default function Home() {
   const [date, setDate] = useState<Date | undefined>(new Date());
   const { setTheme } = useTheme();
 
-  return (
-    <Calendar
-      mode="single"
-      selected={date}
-      onSelect={setDate}
-      className="rounded-md border"
-    />
-  );
+  setTheme("dark");
+
+  return <DatePicker />;
 }
