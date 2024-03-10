bb231a09961028b7cae17f05fce0b2ba05415eb8
eric.cheah <eric.cheah@ufinity.com>
2024-03-10 14:16:33 +0800

    chore: update readme

--- frontend/src/components/ui/weather-card.tsx
+++ frontend/src/components/ui/weather-card.tsx
@@ -18,11 +18,6 @@ export const WeatherCard: React.FC<WeatherCardProps> = ({
   steps,
   isLoading,
 }) => {
-  const images = {
-    cloudy:
-      "https://images.unsplash.com/uploads/14122598319144c6eac10/5f8e7ade?q=80&w=1961&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  };
-
   const renderSteps = (): ReactNode => {
     return steps === Steps.DETAILS ? (
       <div className="h-full w-full flex flex-col items-center justify-center">
@@ -41,15 +36,15 @@ export const WeatherCard: React.FC<WeatherCardProps> = ({
   };
 
   return (
-    <div className="h-[25rem] flex items-center justify-start">
+    <div className="h-[15rem] flex items-center justify-start">
       {isLoading ? (
-        <Skeleton className="h-[25rem]" />
+        <Skeleton className="h-[15rem]" />
       ) : (
         <PinContainer
           title="http://www.weather.gov.sg/"
           href="http://www.weather.gov.sg/"
         >
-          <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
+          <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[10rem] ">
             {weather ? (
               <Fragment>
                 <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
@@ -61,13 +56,6 @@ export const WeatherCard: React.FC<WeatherCardProps> = ({
                     {getNearestQuarter(time!)}
                   </span>
                 </div>
-                <Image
-                  src={images["cloudy"]}
-                  alt="cloudy"
-                  className="flex flex-1 w-full rounded-lg mt-4"
-                  width={300}
-                  height={300}
-                />
               </Fragment>
             ) : (
               renderSteps()
